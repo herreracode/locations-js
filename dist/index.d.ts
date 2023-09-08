182facc36254f6bf638168f8243ae8c8102ce11a
Gerardo Herrera <gerardo.herrera@funiber.org>
2023-09-08 16:50:35 -0500

    add exports and tsconfig

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 import JsonDBLocationRepository from "./Infrastructure/Repositories/JsonDB/JsonDBLocationRepository";
+declare const Repository: JsonDBLocationRepository;
 declare const holaFunct: () => void;
-export { JsonDBLocationRepository, holaFunct };
+export { Repository, holaFunct };
