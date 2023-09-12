2136ade7b9da329f0614ea5d9ea44c7b4d68dcf6
Gerardo Herrera <gerardo.herrera@funiber.org>
2023-09-12 09:46:00 -0500

    feature: conf jest for test

---
+++ test/unit/json-db-location-repository.test.ts
@@ -0,0 +1,19 @@
+import {describe, expect, test, beforeAll, beforeEach} from '@jest/globals';
+
+describe('Json DB repository test', () => {
+
+    beforeAll( () => {
+
+        console.log("asdasdas")
+
+    })
+
+    test('get all countries', () => {
+
+
+
+        expect(1 + 2).toBe(3);
+
+    });
+
+});
