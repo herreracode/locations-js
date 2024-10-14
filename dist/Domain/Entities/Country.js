52e957ed183cffa2566a654dc560a5bdafe43d3a
kevin.aguirre <kevin.aguirre@funiber.org>
2024-10-14 18:18:46 -0500

    Feature: Update dist

--- dist/Domain/Entities/Country.js
+++ dist/Domain/Entities/Country.js
@@ -1,14 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class Country {
-    constructor(id, name, iso2, iso3, translations = null, numericCode = null, phoneCode = null, currency = null, States = null) {
+    constructor(id, name, iso2, iso3, translations = null, phoneCode = null, flagEmoji = null, flagEmojiUnicode = null, numericCode = null, currency = null, States = null) {
         this.id = id;
         this.name = name;
         this.iso2 = iso2;
         this.iso3 = iso3;
         this.translations = translations;
-        this.numericCode = numericCode;
         this.phoneCode = phoneCode;
+        this.flagEmoji = flagEmoji;
+        this.flagEmojiUnicode = flagEmojiUnicode;
+        this.numericCode = numericCode;
         this.currency = currency;
         this.States = States;
     }
